69e2d51b1b021d55795de600f58f6dd338bc3bcb
LauNM <laurie.beduneau@gmail.com>
2022-10-26 09:27:29 +0200

    get profile to be able to pre-fill form with first and last name

--- src/pages/editCredentials.js
+++ src/pages/editCredentials.js
@@ -1,7 +1,7 @@
 import {Link, useNavigate} from "react-router-dom";
-import { useRef } from 'react';
-import { updateProfile } from '../api/apiRquests';
-import {useDispatch} from 'react-redux';
+import { useRef, useEffect } from 'react';
+import {getProfile, updateProfile} from '../api/apiRquests';
+import {useDispatch, useSelector} from 'react-redux';
 
 import { setUser } from '../slices/userSlice';
 
@@ -22,6 +22,22 @@ function EditCredentials() {
     }
   }
 
+  const fetchUser = async () => {
+    try {
+      const { body } = await getProfile();
+      dispatch(setUser(body));
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  },[]);
+
+  const user = useSelector((state) => state.user)
+
   return (
     <div className="update-credentials">
       <h2>Modifier mes informations</h2>
@@ -29,11 +45,11 @@ function EditCredentials() {
         <form onSubmit={handleSubmit}>
         <div className="input-wrapper">
           <label htmlFor="firstname">Prénom</label>
-          <input ref={firstNameRef} type="text" id="firstname" required={true}/>
+          <input ref={firstNameRef} type="text" id="firstname" required={true} defaultValue={user.firstName}/>
         </div>
         <div className="input-wrapper">
           <label htmlFor="lastname">Nom</label>
-          <input ref={lastNameRef} type="text" id="lastname" required={true}/>
+          <input ref={lastNameRef} type="text" id="lastname" required={true} defaultValue={user.lastName}/>
         </div>
         <button className="sign-in-button">Modifier</button>
       </form>
